0a7d75436f3cb26646967e45624bb66bd5da5871
kevinbasoftcat <kevinba@softcat.com>
2022-02-16 10:23:20 +0000

    Updatedlocation short name variable condition

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "locationshortname" {
   type = string
   description = "Location three letter abbreviation"
   validation {
-      condition = length (var.resource_prefix) == 3
+      condition = length (var.locationshortname) == 3
       error_message = "The location shortname must be only 3 characters long."
     }
 }
