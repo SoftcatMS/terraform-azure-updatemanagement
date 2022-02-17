de8046e7d28ddb8b1f04b70a5f28bf2b3a54d195
kevinbasoftcat <kevinba@softcat.com>
2022-02-17 12:54:02 +0000

    Updated name to include test

--- tests/basic/main.tf
+++ tests/basic/main.tf
@@ -1,9 +1,9 @@
 module "updatemanagement" {
   source                        = "git@github.com:SoftcatMS/azure-terraform-updatemanagement"
   location                      = "uksouth"
-  resource_group_name           = "rg-example-updatemanagement"
-  automation_account_name       = "example-auto-acct"
-  log_analytics_workspace_name  = "example-log-workspace"
+  resource_group_name           = "rg-test-updatemanagement"
+  automation_account_name       = "test-auto-acct"
+  log_analytics_workspace_name  = "test-log-workspace"
 
   tags = {
     environment = "test"
