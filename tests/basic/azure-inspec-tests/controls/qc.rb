e0fcc2ea78d9d4c18e104daf4654302862d4d90b
kevinbasoftcat <kevinba@softcat.com>
2022-02-17 14:58:15 +0000

    Added additional tests

--- tests/basic/azure-inspec-tests/controls/qc.rb
+++ tests/basic/azure-inspec-tests/controls/qc.rb
@@ -2,24 +2,12 @@
 
 # Test values
 
-resource_group1 = 'rg-example-updatemanagement'
+resource_group1 = 'rg-test-updatemanagement'
 
 describe azure_generic_resources(resource_group: resource_group1) do
-  its('names') { should include('example-auto-acct') }
+  its('names') { should include('test-auto-acct') }
 end
 
-# describe azure_generic_resource(resource_group: resource_group1, name: 'vpn-test-basic-gw') do
-#   its('properties.sku.name') { should cmp 'VpnGw1AZ' }
-#   its('properties.vpnType') { should cmp 'RouteBased' } 
-#   its('properties.provisioningState') { should cmp 'Succeeded' } 
-#   its('properties.packetCaptureDiagnosticState') { should cmp 'None' }
-#   its('properties.enablePrivateIpAddress') { should cmp 'false' }
-#   its('properties.enableBgpRouteTranslationForNat') { should cmp 'false' }
-#   its('properties.enableBgp') { should cmp 'false' }
-#   its('properties.gatewayType') { should cmp 'Vpn' }
-#   its('properties.activeActive') { should cmp 'false' }
-#   its('properties.vpnGatewayGeneration')  { should cmp 'Generation1' }
-#   # its('properties.ipConfigurations') { should cmp '' }
-#   # its('properties.natRules') { should cmp '' }
-#   # its('properties.bgpSettings') { should cmp '' }
-# end
+describe azure_generic_resources(resource_group: resource_group1) do
+  its('names') { should include('test-log-workspace') }
+end
