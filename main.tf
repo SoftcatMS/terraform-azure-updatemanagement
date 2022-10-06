267cff9a8e76d993f85d624c3dd12448aa4ac0c1
kevinbasoftcat <kevinba@softcat.com>
2022-10-06 12:12:35 +0100

    Added lifecycle ignore for tags

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 data "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   name =  var.log_analytics_workspace_name
   resource_group_name = var.log_analytics_resource_group_name
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
 }
 
 resource "random_string" "random_string" {
